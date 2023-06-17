925335a0d4ea17c5a7f76c0697f78a4e57c7755c
Hasan Hafizur Rahman <hasanshanto922@gmail.com>
2023-06-18 01:13:00 +0600

    contact info

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Sidebar from "./pages/global/Sidebar";
 import Dashboard from "./pages/dashboard";
 import Team from "./pages/team";
 // import Invoices from "./pages/invoices";
-// import Contacts from "./pages/contacts";
+import Contacts from "./pages/contacts";
 // import Bar from "./pages/bar";
 // import Form from "./pages/form";
 // import Line from "./pages/line";
@@ -27,8 +27,8 @@ function App() {
             <Routes>
               <Route path="/" element={<Dashboard />} />
               <Route path="/team" element={<Team />} />
-              {/* <Route path="/contacts" element={<Contacts />} />
-            <Route path="/invoices" element={<Invoices />} />
+              <Route path="/contacts" element={<Contacts />} />
+              {/*<Route path="/invoices" element={<Invoices />} />
             <Route path="/form" element={<Form />} />
             <Route path="/bar" element={<Bar />} />
             <Route path="/pie" element={<Pie />} />
