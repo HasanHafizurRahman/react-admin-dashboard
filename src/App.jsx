f96a5b6068f77164924b2b7f5435e89f17700099
Hasan Hafizur Rahman <hasanshanto922@gmail.com>
2023-06-18 00:53:17 +0600

    mui data-grid

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { ColorModeContext, useMode } from "./theme";
 import Topbar from "./pages/global/Topbar";
 import Sidebar from "./pages/global/Sidebar";
 import Dashboard from "./pages/dashboard";
-// import Team from "./pages/team";
+import Team from "./pages/team";
 // import Invoices from "./pages/invoices";
 // import Contacts from "./pages/contacts";
 // import Bar from "./pages/bar";
@@ -26,8 +26,8 @@ function App() {
             <Topbar />
             <Routes>
               <Route path="/" element={<Dashboard />} />
-              {/* <Route path="/team" element={<Team />} />
-            <Route path="/contacts" element={<Contacts />} />
+              <Route path="/team" element={<Team />} />
+              {/* <Route path="/contacts" element={<Contacts />} />
             <Route path="/invoices" element={<Invoices />} />
             <Route path="/form" element={<Form />} />
             <Route path="/bar" element={<Bar />} />
--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,7 +1,23 @@
-
+import { Box, Button, IconButton, Typography, useTheme } from "@mui/material";
+import { tokens } from "../../theme";
+import { mockTransactions } from "../../data/mockData";
+import DownloadOutlinedIcon from "@mui/icons-material/DownloadOutlined";
+import EmailIcon from "@mui/icons-material/Email";
+import PointOfSaleIcon from "@mui/icons-material/PointOfSale";
+import PersonAddIcon from "@mui/icons-material/PersonAdd";
+import TrafficIcon from "@mui/icons-material/Traffic";
+import Header from "../../components/Header";
 
 const Dashboard = () => {
-    return <div>Dashboard</div>;
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  return (
+    <Box m="20px">
+      <Box display="flex" justifyContent="space-between" alignItems="center">
+        <Header title="Dashboard" subtitle="Welcome to your Dashboard" />
+      </Box>
+    </Box>
+  );
 };
 
 export default Dashboard;
