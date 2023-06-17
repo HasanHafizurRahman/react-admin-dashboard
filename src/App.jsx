0b080f46c602ab7441bc12ce59682e031eb5eeb7
Hasan Hafizur Rahman <hasanshanto922@gmail.com>
2023-06-17 18:38:23 +0600

    react-pro-sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,18 @@
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./theme";
 import Topbar from "./pages/global/Topbar";
+import Sidebar from "./pages/global/Sidebar";
+import Dashboard from "./pages/dashboard";
+// import Team from "./pages/team";
+// import Invoices from "./pages/invoices";
+// import Contacts from "./pages/contacts";
+// import Bar from "./pages/bar";
+// import Form from "./pages/form";
+// import Line from "./pages/line";
+// import Pie from "./pages/pie";
+// import FAQ from "./pages/faq";
+// import Geography from "./pages/geography";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -8,9 +20,25 @@ function App() {
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <main className="content">
-          <Topbar />
-        </main>
+        <div className="app">
+          <Sidebar />
+          <main className="content">
+            <Topbar />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              {/* <Route path="/team" element={<Team />} />
+            <Route path="/contacts" element={<Contacts />} />
+            <Route path="/invoices" element={<Invoices />} />
+            <Route path="/form" element={<Form />} />
+            <Route path="/bar" element={<Bar />} />
+            <Route path="/pie" element={<Pie />} />
+            <Route path="/line" element={<Line />} />
+            <Route path="/faq" element={<FAQ />} /> */}
+              {/* <Route path="/calendar" element={<Calendar />} /> */}
+              {/* <Route path="/geography" element={<Geography />} /> */}
+            </Routes>
+          </main>
+        </div>
       </ThemeProvider>
     </ColorModeContext.Provider>
   );
